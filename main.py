c1ae6862417686e8b80d3c682f76eafe4432cd78
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-30 11:44:08 +0900

    모드가 두개가 됐어요

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pico2d import open_canvas, delay, close_canvas
-import logo_mode
+import logo_mode as start_mode
+import title_mode
 import game_framework
 
+
 open_canvas()
-game_framework.run(logo_mode)
+game_framework.run(start_mode)
 close_canvas()
